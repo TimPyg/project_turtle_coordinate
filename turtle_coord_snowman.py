c7d8f876b898a88b475e315aefdc62cb848fa624
TimPyg <99884430+TimPyg@users.noreply.github.com>
2022-02-17 15:56:54 +0300

    Add files via upload

---
+++ turtle_coord_snowman.py
@@ -0,0 +1,19 @@
+import turtle
+t = turtle.Pen()
+turtle.bgcolor('blue')
+t.color('yellow')
+
+#body
+t.penup()
+t.goto(0,-140)
+t.pendown()
+t.goto(40,-140)
+t.goto(60,-120)
+t.goto(60,-60)
+t.goto(40,-40)
+t.goto(50,-30)
+t.goto(50,20)
+t.goto(35,35)
+
+
+turtle.done()
